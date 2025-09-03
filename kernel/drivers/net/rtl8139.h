f786acf5f27190cd8fc262c11c16e97ba664e9e0
GlitchedCode922 <126592994+GlitchedCode922@users.noreply.github.com>
2025-09-03 20:06:11 +0300

    Add `rtl8139_read_packet()` to the header file

--- kernel/drivers/net/rtl8139.h
+++ kernel/drivers/net/rtl8139.h
@@ -5,3 +5,4 @@ void rtl8139_init(pci_device_t device);
 void rtl8139_irq_handler(uint8_t irq);
 void turn_on_rtl8139(int card);
 void register_rtl8139_driver();
+int rtl8139_read_packet(int card, void** buffer);
