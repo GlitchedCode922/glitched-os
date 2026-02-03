43f6bf64a7d69e99dcc7a12023112009f106948e
GlitchedCode922 <georgepap5671@gmail.com>
2026-02-03 18:42:18 +0200

    Add option to reboot with Enter on kernel panic

--- kernel/panic.h
+++ kernel/panic.h
@@ -1,23 +1,25 @@
 #pragma once
 #include "console.h"
 #include <stdarg.h>
+#include "power.h"
+#include "drivers/ps2_keyboard.h"
 
 static inline __attribute__((noreturn)) void panic(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    
+
     clear_screen();
-    
+
     // Set background color to black for panic messages
     setbg_color(COLOR(0, 0, 0));
-    
+
     // Print the panic message
     kprintf(":( Kernel panic: ");
     kvprintf(fmt, args);
-    
-    while (1) {
-        asm volatile("hlt");
-    }
-    
+    kprintf("\n\nPress Enter to reboot...");
+
+    get_input_line();
+    reboot();
+
     va_end(args);
 }
