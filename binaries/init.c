a8962936acbaff7347fab68354922c858b123c0e
GlitchedCode922 <georgepap5671@gmail.com>
2026-06-25 16:31:30 +0300

    Add /tmp and automatically mount it

--- binaries/init.c
+++ binaries/init.c
@@ -3,6 +3,7 @@
 
 int main() {
     mount(NULL, "/dev", "devfs", 0);
+    mount(NULL, "/tmp", "ramfs", 0);
     int console_fd = open("/dev/tty1", 0);
     dup2(console_fd, STDIN_FILENO);
     dup2(console_fd, STDOUT_FILENO);
