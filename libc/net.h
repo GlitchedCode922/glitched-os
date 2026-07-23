b0d83c452b140beaf5aa2d4eb3633d49abdfcb0c
GlitchedCode922 <georgepap5671@gmail.com>
2026-07-23 18:45:23 +0300

    Add ntohs/ntohl/htons/htonl to libc/net.h

--- libc/net.h
+++ libc/net.h
@@ -14,6 +14,28 @@ typedef struct {
     uint16_t port;
 } __attribute__((packed)) sockaddr_in_t;
 
+static inline uint16_t htons(uint16_t val) {
+    return (val >> 8) | (val << 8);
+}
+
+static inline uint16_t ntohs(uint16_t val) {
+    return (val >> 8) | (val << 8);
+}
+
+static inline uint32_t htonl(uint32_t val) {
+    return ((val >> 24) & 0x000000FF) |
+           ((val >> 8)  & 0x0000FF00) |
+           ((val << 8)  & 0x00FF0000) |
+           ((val << 24) & 0xFF000000);
+}
+
+static inline uint32_t ntohl(uint32_t val) {
+    return ((val >> 24) & 0x000000FF) |
+           ((val >> 8)  & 0x0000FF00) |
+           ((val << 8)  & 0x00FF0000) |
+           ((val << 24) & 0xFF000000);
+}
+
 int ping(uint8_t* dest_ip);
 int configure_network_interface_static(int index, uint32_t ip, uint32_t subnet, uint32_t router);
 void add_route(uint8_t* dest_ip, uint8_t* gateway, uint8_t* netmask, int card);
